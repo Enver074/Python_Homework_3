3ac65206245456e97c67bba0ddd16efab7be849a
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2022-12-23 14:58:07 +0300

    Второе задание

--- seminar_3.py
+++ seminar_3.py
@@ -4,16 +4,44 @@
 
 # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
+# from random import randint
+
+# n = int(input('Введите размер массива: '))
+# numb = []
+# for i in range(n):
+#     numb.append(randint(0, 10))
+# sum = 0
+# for i in range(1, len(numb), 2):
+#     sum += numb[i]
+#     print(sum)
+
+# print(f'Массив {numb}')
+# print(f'Сумма нечетных элементов равна: {sum}')
+
+# Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+# Пример:
+
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+
 from random import randint
 
-n = int(input('Введите размер массива: '))
+n = int(input('Введите размер списка: '))
 numb = []
+mult = []
+
 for i in range(n):
     numb.append(randint(0, 10))
-sum = 0
-for i in range(1, len(numb), 2):
-    sum += numb[i]
-    print(sum)
 
-print(f'Массив {numb}')
-print(f'Сумма нечетных элементов равна: {sum}')
+for i in range(len(numb)):
+    while i < len(numb)/2 and n > len(numb)/2:
+        n = n-1
+        m = numb[i] * numb[n]
+        mult.append(m)
+        i+=1
+
+print(numb)
+print(mult)
+
