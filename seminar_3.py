8ddefcc145b8a099f0669e07cc3efd79ea3c719a
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2022-12-23 18:07:11 +0300

    Пятое задание

--- seminar_3.py
+++ seminar_3.py
@@ -84,14 +84,33 @@
 # - 3 -> 11
 # - 2 -> 10
 
-n = int(input('Введите число: '))
+# n = int(input('Введите число: '))
 
 # bin_d = bin(n)[2:]
 # print(bin_d)
 
-a = ''
+# a = ''
 
-while n > 0:
-    a = str(n % 2) + a
-    n = n // 2 
-print(a)
+# while n > 0:
+#     a = str(n % 2) + a
+#     n = n // 2 
+# print(a)
+
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+
+# Пример:
+
+# - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] [Негафибоначчи]
+
+n = int(input('Введите число: '))
+
+fibonacci = [0,1]
+negative = [0,1]
+
+for i in range(n-1):
+    fibonacci.append(fibonacci[-2] + fibonacci[-1])
+
+for i in range(n-1):
+    negative.append(negative[-2] - negative[-1])
+
+print(negative[::-1] + fibonacci[1:])
